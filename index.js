70235c3d1c321bed58120ab2ca4fa85f193a84c2
ASHISHBISHT-gi <syntanalprime@gmail.com>
2025-07-20 21:20:58 +0530

    desktop dev completed_1.1

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-const buttons=document.querySelectorAll('.buttons>button');
-const submit=document.querySelector('button[type="submit"]');
-const submition=document.querySelector(".submition");
-const main=document.querySelector("main");
-const body=document.querySelector("body");
-const span=document.querySelector("span");
+const buttons = document.querySelectorAll('.buttons>button');
+const submit = document.querySelector('button[type="submit"]');
+const submition = document.querySelector(".submition");
+const main = document.querySelector("main");
+const body = document.querySelector("body");
+const span = document.querySelector("span");
 body.removeChild(submition);
-let rating=0;
+let rating = 0;
+function star(button) {
+    console.log(buttons);
+    // console.log(parseInt(button.innerText));
+    // let counter=par
+    // button.classList.add="button_hover";
+    rating = parseInt(button.innerText);
+    for (let i = 0; i < parseInt(button.innerText); i++) {
+        buttons[i].style.color = "black";
+        buttons[i].style.backgroundColor = "white";
+    }
+    for (let i = parseInt(button.innerText); i < 5; i++) {
+        buttons[i].style.color = "white";
+        buttons[i].style.backgroundColor = "hsl(215, 18%, 23%)";
+    }
+}
 buttons.forEach(button => {
-    button.addEventListener('click',(e)=>{
-        console.log(buttons);
-        // console.log(parseInt(button.innerText));
-        // let counter=par
-        // button.classList.add="button_hover";
-        rating=parseInt(button.innerText);
-        for(let i=0;i<parseInt(button.innerText);i++){
-            buttons[i].style.color="black";
-        buttons[i].style.backgroundColor="white";
-        }
-        for(let i=parseInt(button.innerText);i<5;i++){
-            buttons[i].style.color="white";
-            buttons[i].style.backgroundColor="hsl(215, 18%, 23%)";
-        }
+    button.addEventListener('click', (e) => {
+        star(button);
     })
+    button.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+            star(button);
+        }
 });
-submit.addEventListener('click',(e)=>{
-    submit.style.backgroundColor="hsl(0, 100%, 100%)";
-    submit.style.color="hsl(216, 12%, 8%)";
-    span.innerText=rating;
+});
+submit.addEventListener('click', (e) => {
+    submit.style.backgroundColor = "hsl(0, 100%, 100%)";
+    submit.style.color = "hsl(216, 12%, 8%)";
+    span.innerText = rating;
     body.removeChild(main);
     body.appendChild(submition);
 });
